bc389795110270178d1896972704a4b204a28dc9
mithun babbira <mithunbabbira@gmail.com>
2020-06-07 15:39:39 +0530

    update

--- app/src/main/java/com/example/uploadandretrivefromfirebase/ImageActivity.java
+++ app/src/main/java/com/example/uploadandretrivefromfirebase/ImageActivity.java
@@ -1,14 +1,40 @@
 package com.example.uploadandretrivefromfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImageActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private  ImageAdapter mAdapter;
+
+
+    private DatabaseReference mDatabaseRef;
+    private List<Upload> mUploads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mUploads = new ArrayList<>();
+
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+
     }
 }
---
+++ app/src/main/java/com/example/uploadandretrivefromfirebase/ImageAdapter.java
@@ -0,0 +1,64 @@
+package com.example.uploadandretrivefromfirebase;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<Upload> mUpload;
+
+
+    public  ImageAdapter (Context context,List<Upload> uploads){
+        mContext = context;
+        mUpload = uploads;
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item,parent,false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        Upload uploadCurrent = mUpload.get(position);
+        holder.textViewName.setText(uploadCurrent.geName());
+        Picasso.with(mContext).load(uploadCurrent.getImageUrl()).fit().centerCrop().into(holder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUpload.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName;
+        public  ImageView imageView;
+
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            imageView = itemView.findViewById(R.id.image_view_upload);
+        }
+    }
+}
